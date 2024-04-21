0e096620878c5f55d7e388842e51579bec59a515
Israel Fernandez <israelfernandez96@gmail.com>
2024-04-20 19:54:54 -0700

    Added Javadocs

--- src/main/java/org/example/Attack.java
+++ src/main/java/org/example/Attack.java
@@ -1,5 +1,7 @@
 package org.example;
-
+/**
+ * An interface for the attack ability.
+ */
 public interface Attack extends Ability{
   Integer attack(Monster monster);
 }
--- src/main/java/org/example/RangedAttack.java
+++ src/main/java/org/example/RangedAttack.java
@@ -2,9 +2,20 @@ package org.example;
 
 public class RangedAttack implements Attack{
   Monster attacker;
+
+  /**
+   * Constructor for RangedAttack.
+   * @param attacker
+   */
   public RangedAttack(Monster attacker) {
     this.attacker = attacker;
   }
+
+  /**
+   * Attack method for RangedAttack.
+   * @param target
+   * @return Integer
+   */
   @Override
   public Integer attack(Monster target) {
     String message = attacker + " uses a ranged attack on " + target;
